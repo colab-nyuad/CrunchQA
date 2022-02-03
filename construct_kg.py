5d46effa37f898322c6ed6b1d88a8b42f4c8628d
Silvester Yu <ly1164@nyu.edu>
2022-02-03 11:06:55 +0400

    now ablt to delete previous output files

--- construct_kg.py
+++ construct_kg.py
@@ -12,6 +12,7 @@ from utils import *
 from sklearn import preprocessing
 import math
 import json
+import os, shutil
 
 clusters = {}
 organizations_list = []
@@ -24,6 +25,21 @@ vanilla_data = 'vanilla'
 clustering_data = 'clustering'
 regression_data = 'regression'
 min_max_scaler = preprocessing.MinMaxScaler()
+
+
+# clear previous contents
+folders = ["vanilla/", "clustering/", "regression/", "triples/"]
+for folder in folders:
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+print("----------all previous kg files cleared----------")
 
 
 executive_jobs = {'president' : ['president'],
